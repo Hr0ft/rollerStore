023bf758a3303c8ffd4166fc1b527f73b5f38856
Pavel Kazantsev <tiny0886@mail.ru>
2023-02-11 18:09:06 +0300

    udate a project structure, upload scss

---
+++ src/data/category.data.ts
@@ -0,0 +1,11 @@
+export const productsCategoryData = [
+  { id: 1, name: 'Ролы' },
+  { id: 2, name: 'лед коньки' },
+  { id: 3, name: 'самокаты' },
+  { id: 4, name: 'Вело' },
+  { id: 5, name: 'защита' },
+  { id: 6, name: 'аксесс' },
+  { id: 7, name: 'запчасти' },
+  { id: 8, name: 'скейты' },
+  { id: 9, name: 'одежда' },
+];
---
+++ src/data/product.data.ts
@@ -0,0 +1,14 @@
+import { IProduct } from '../types/product.interface';
+
+export const products: IProduct[] = [
+  {
+    id: 1,
+    name: 'first Product',
+    price: 100,
+  },
+  {
+    id: 2,
+    name: 'second Product',
+    price: 100,
+  },
+];
